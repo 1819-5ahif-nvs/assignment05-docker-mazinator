9ab02d49316b5ee0fa68122539cfce499e6452e8
mazinator <daniel.maz99@gmail.com>
2018-11-30 13:37:12 +0100

    some changes

--- MySQL/setup.sql
+++ MySQL/setup.sql
@@ -1,1 +1,1 @@
-create database myDB;
+create database PetDS;
---
+++ Wildfly/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# wait until MySQL is really available
+maxcounter=60
+ 
+counter=1
+while ! mysql -h mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" -e "show databases;"; do
+    echo "waiting for Godot..."
+    sleep 1
+    counter=`expr $counter + 1`
+    if [ $counter -gt $maxcounter ]; then
+        >&2 echo "We have been waiting for MySQL too long already; failing."
+        exit 1
+    fi;
+done
+echo "yeah, mysql available, so lets go..."
+java -jar /opt/microprofile/microprofile.jar
